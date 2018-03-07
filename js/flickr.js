9c5c016b88f194471df5a0c8cab5eb39340212ac
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-07 23:27:56 +0530

    search bar added ~ finally shit works as intended

--- js/flickr.js
+++ js/flickr.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
-  $('button').click(function() {
-    // $(this) indicated the element that we clicked, only that button which is clicked
-    $('button').removeClass('selected');
-    $(this).addClass('selected');
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $(this).text();
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items, function(i, photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      });
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
-  }) // button click
-}); // end ready  
+  
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+ 
+ 
+  $('form').submit(function (evt) {
+
+     var $submitButton = $('#submit');
+     var $searchField = $('#search');
+
+     evt.preventDefault();
+
+     $searchField.prop("disabled",true);
+     $submitButton.attr("disabled", true).val("searching....");
+     
+     var animal = $searchField.val();
+     
+     $('#photos').html('');
+
+     $.getJSON(flickerAPI, {
+         tags: animal,
+         format: "json"
+       },
+     function(data){
+       var photoHTML = '';
+       if (data.items.length > 0) {
+         $.each(data.items,function(i,photo) {
+           photoHTML += '<li class="grid-25 tablet-grid-50">';
+           photoHTML += '<a href="' + photo.link + '" class="image">';
+           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+         }); // end each
+       } else {
+         photoHTML = "<p>No photos found that match: " + animal + ".</p>"
+       }
+       $('#photos').html(photoHTML);
+       $searchField.prop("disabled", false);
+       $submitButton.attr("disabled", false).val("Search");
+     }); // end getJSON
+ 
+   }); // end click
+ 
+ }); // end ready
