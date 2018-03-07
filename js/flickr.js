fbddfe304b79ff9e04dae0674bda3f6c4eaf39ab
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-07 22:10:14 +0530

    $.getJSON( ) skelton & Flickr API

--- js/flickr.js
+++ js/flickr.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
     // $(this) indicated the element that we clicked, only that button which is clicked
     $('button').removeClass('selected');
     $(this).addClass('selected');
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text();
+    var flickrOptions = {
+      tags: animal,
+      format: "json"
+    };
+    function displayPhotos(data) {
+      
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
   }) // button click
 }); // end ready  
