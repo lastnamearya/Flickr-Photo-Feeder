c4dbb7051590347594d152ccc4699132c5a01760
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-07 21:35:21 +0530

    button class change on click

---
+++ js/flickr.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('button').click(function() {
+    // $(this) indicated the element that we clicked, only that button which is clicked
+    $(this).addClass('selected');
+  }) // button click
+}); // end ready  
