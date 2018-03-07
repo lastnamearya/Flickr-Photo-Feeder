de09e92fe5661419bd77019d9a47db462f2f555c
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-07 21:37:10 +0530

    button fixed for all

--- js/flickr.js
+++ js/flickr.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('button').click(function() {
     // $(this) indicated the element that we clicked, only that button which is clicked
+    $('button').removeClass('selected');
     $(this).addClass('selected');
   }) // button click
 }); // end ready  
